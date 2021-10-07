0efa7b41ca0731734b5f5b390c7e8dc20e25fb5a
Chris Concannon <chris@concannon.tech>
2021-10-06 21:32:18 -0700

    add license and CreatePath()

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/cconcannon/gpx2czml
 go 1.16
 
 require (
-	github.com/cconcannon/czml v0.0.7
+	github.com/cconcannon/czml v0.0.8
 	github.com/cconcannon/gpx v0.0.3
 )
+
+retract v0.0.1
--- gpx2czml.go
+++ gpx2czml.go
@@ -1,6 +1,8 @@
 package gpx2czml
 
 import (
+	"strings"
+
 	"github.com/cconcannon/czml"
 	"github.com/cconcannon/gpx"
 )
@@ -29,11 +31,40 @@ func CreatePolyline(id, name string, g gpx.Gpx) czml.Czml {
 	return c
 }
 
+func CreatePath(id, name string, g gpx.Gpx) czml.Czml {
+	var c czml.Czml
+
+	initializeCzml(&g, &c)
+
+	p := czml.CreateEmptyPacket(id, name)
+
+	p.AddPath("white")
+	p.AddBillboard()
+
+	tracks := g.GetTracks()
+	segments := tracks[0].GetSegments()
+	points := segments[0].GetTrackPoints()
+
+	for _, point := range points {
+		p.AddPosition(point.Time, point.Lat, point.Lon, point.Elevation)
+	}
+
+	startTime := points[0].Time
+	stopTime := points[len(points)-1].Time
+	interval := strings.Join([]string{startTime, stopTime}, "/")
+
+	c.AddPacket(p)
+	c.AddClock(interval, startTime, 50)
+
+	return c
+}
+
 // transfer metadata from gpx, if it exists
 func initializeCzml(g *gpx.Gpx, c *czml.Czml) {
-	docName := g.Metadata.Name
-
-	if g.Metadata.Name == "" {
+	var docName string
+	if g.Metadata != nil {
+		docName = g.Metadata.Name
+	} else {
 		docName = "GPX2CZML Document"
 	}
 
